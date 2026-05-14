e1687ba43c157ffcc0d1e2ed58780010ce653a27
Enrico Carbonari <enricocarrr@gmail.com>
2026-05-13 21:39:49 -0300

    começo do feedback nos glitchs
    particulas cagadas

--- Assets/Script/VidaGlitch.cs
+++ Assets/Script/VidaGlitch.cs
@@ -4,35 +4,29 @@ using UnityEngine;
 public class VidaGlitch : MonoBehaviour, IDamageable
 {
     [Header("Configurações")]
-    public int vidaMaxima = 3;
-    private int vidaAtual;
+    private int vidaGlitch = 1;
+    [Header("Efeitos")]
+    [SerializeField] private GameObject particulaPrefab;
 
-    void Start()
+    public void TakeDamage(int dano)
     {
-        vidaAtual = vidaMaxima;
-    }
-
-    // Este é o método que a Interface exige. 
-    // Quando o Player chamar TakeDamage, ele vai executar o seu ReceberDano.
-    public void TakeDamage(int amount)
-    {
-        ReceberDano(amount);
-    }
-
-    public void ReceberDano(int dano)
-    {
-        vidaAtual -= dano;
-        Debug.Log("Glitch tomou dano! Vida restante: " + vidaAtual);
-
-        if (vidaAtual <= 0)
-        {
-            Quebrar();
-        }
+        vidaGlitch -= dano;
+         Quebrar();
     }
 
     private void Quebrar()
     {
         Debug.Log("Glitch destruído!");
+        if (particulaPrefab != null)
+        {
+            // Cria as partículas na posição do objeto
+            GameObject particulas = Instantiate(particulaPrefab, transform.position, Quaternion.identity);
+
+            // Opcional: Se o seu prefab não tiver o "Stop Action" como Destroy, 
+            // você força a destruição dele após 2 segundos para não pesar o jogo
+            Destroy(particulas, 2f);
+        }
         Destroy(gameObject);
+        
     }
 }
