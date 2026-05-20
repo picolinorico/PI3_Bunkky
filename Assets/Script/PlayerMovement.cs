23948b8e41e73e47979b6a75f7bacdfd3792b2b0
Enrico Carbonari <enricocarrr@gmail.com>
2026-05-20 17:43:18 -0300

    puta que pariu.
    desculpa ryan

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -59,7 +59,7 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private int deadEnemies = 0;
     [SerializeField] private int deathsForUpgrades = 3;
     [SerializeField] private Vector2 sizeUpgrade = new Vector2(8f, 2f); // Alcance maior
-    //[SerializeField] private int danoUpgrade = 2;
+    [SerializeField] private int danoUpgrade = 2;
     private bool isUpgraded = false;
 
     [Header("Knockback")]
@@ -292,7 +292,7 @@ public class PlayerMovement : MonoBehaviour
         {
             isUpgraded = true;
             attackSize = sizeUpgrade;
-            //attackDamage = danoUpgrade;
+            attackDamage = danoUpgrade;
             Debug.Log("ATAQUE MELHORADO PERMANENTE!");
             // Aqui você pode instanciar uma partícula de brilho na coelha se quiser
             if (interfaceDeUpgrades != null) interfaceDeUpgrades.LigarIconeAtaque();
